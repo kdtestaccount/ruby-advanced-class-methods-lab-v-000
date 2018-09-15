192102f2f858d7884846f3ea6e168910dc1fa845
kvdoherty <kvdoherty@gmail.com>
2018-09-15 15:20:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   end
   
   def self.new_from_filename(file_name)
-    file_name.tr('.mp3', '')
+    new_file_namefile_name.tr('.mp3', '')
     person.name = file_name.split(" - ").collect{|w| w.capitalize}.join(" ")
     
     
